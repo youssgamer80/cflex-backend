1ce6ee4cb99b92ce31192b8a2169f676edfcf57c
angVpat <angepatrickgoprou@gmail.com>
2022-05-09 09:58:15 +0000

    Modification de proprietaire controller

--- src/main/java/projet/cflex/oda_cflex_smart_city1/Controller/ProprietaireController.java
+++ src/main/java/projet/cflex/oda_cflex_smart_city1/Controller/ProprietaireController.java
@@ -14,58 +14,48 @@ import java.util.List;
 @RestController
 public class ProprietaireController {
     @Autowired
-    ProprietaireService suppproprietaireService;
-    @Autowired
-    ProprietaireService majproprietaireService;
-    @Autowired
-    ProprietaireService editproprietaire;
-    @Autowired
-    ProprietaireService infoproprio;
-    @Autowired
-    private ProprietaireRepository proprietaireRepository;
+    ProprietaireService proprietaireService;
 
     @GetMapping("/listeproprietaire")
     @ResponseBody
     public List<Proprietaire> ListeProprio(ModelMap modelMap){
-        List <Proprietaire> listeproprio = proprietaireRepository.findAll();
+        List <Proprietaire> listeproprio = proprietaireService.findAll();
         return listeproprio;
     }
 
     //Endpoint retournant la liste des infos pour un propriétaire donné
-   @GetMapping("/listeinfoproprietaire/{id}")
+    @GetMapping("/listeinfoproprietaire/{id}")
     @ResponseBody
     public Object listinfoproprio(@Validated @PathVariable("id") Integer id) {
         try {
-            Proprietaire infoproprietaire = infoproprio.findOne(id)
+            Proprietaire infoproprietaire = proprietaireService.findOne(id)
                     .orElseThrow(() -> new IllegalArgumentException("Id invalide" + id));
 
             return (infoproprietaire);
         }
         catch (Exception e){
-          return ("Le propriétaire aved l'Id:"+id+" n'existe pas");
+            return ("Le propriétaire aved l'Id:"+id+" n'existe pas");
         }
-   }
+    }
 
     @PostMapping(value = "/addproprio")
     public String addProprio(@ModelAttribute("proprio")@Validated Proprietaire proprietaire, BindingResult bindingResult){
-        proprietaireRepository.save(proprietaire);
+        proprietaireService.save(proprietaire);
         return ("Le proprietaire a été ajouté avec succès");
     }
 
 
     @DeleteMapping("/suppproprietaire/{id}")
     public String suppproprietaire( @PathVariable Integer id){
-        suppproprietaireService.delete(id);
+        proprietaireService.delete(id);
         return ("Le propriétaire  "+id+" a été supprimé avec succès");
     }
 
-
-
-    @GetMapping("/modifproprio/{id}")
+    @PostMapping("/modifproprio/{id}")
     @ResponseBody
     public String modifproprietaire(@PathVariable("id") Integer id) {
         try{
-            editproprietaire.findOne(id)
+            proprietaireService.findOne(id)
                     .orElseThrow(() -> new IllegalArgumentException("Id invalide:" + id));
 
             return("La modification des informations du proprietaire "+id+"a été effectuée");
