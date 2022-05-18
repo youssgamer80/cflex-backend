e1758b3d05f7b79fb26e884a9d32c709699d6e1b
Lauriat Herosside <laulitadeo@gmail.com>
2022-05-18 14:21:03 +0000

    Page De Connexion

--- src/main/java/projet/cflex/oda_cflex_smart_city1/OdaCflexSmartCity1Application.java
+++ src/main/java/projet/cflex/oda_cflex_smart_city1/OdaCflexSmartCity1Application.java
@@ -10,13 +10,11 @@ import io.swagger.v3.oas.annotations.info.Info;
 
 @SpringBootApplication
 
-@OpenAPIDefinition(info =
-	@Info(title = "C'Flex API", version = "${project.version}", description = "Documentation de l'API de C'Flex v1.0")
-)
+@OpenAPIDefinition(info = @Info(title = "C'Flex API", version = "${project.version}", description = "Documentation de l'API de C'Flex v1.0"))
 public class OdaCflexSmartCity1Application {
 
-    public static void main(String[] args) {
-      SpringApplication.run(OdaCflexSmartCity1Application.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(OdaCflexSmartCity1Application.class, args);
+  }
 
 }
